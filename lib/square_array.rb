7627ba8a2bc4d9c132b920f3587c9c4ee0144b87
Samuel Lesser <ineedmoreplates@gmail.com>
2020-02-14 22:38:17 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,14 @@
-def square_array(array)
-  # your code here
+
+def square_array(numbers)
+  i = 0
+  sum = []
+  
+  while numbers[i] do
+    puts sum << numbers[i] ** 2
+          i += 1
+  end
+sum
+
 end
+
+
